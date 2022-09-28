bf360d696142e366c76f4098280387c068fdf6a3
notsamdonald <sdonald.uc@gmail.com>
2022-09-27 22:02:13 -0400

    Finalizing emissions and covid-19_behaviours datasets for corgis merge

--- processed/covid_behaviors/covid_behaviors-processor.py
+++ processed/covid_behaviors/covid_behaviors-processor.py
@@ -2,10 +2,12 @@ import pandas as pd
 
 
 def main():
-    # dataset_name = 'YouGov-Imperial COVID-19 Behavior Tracker'
-    dataset_name = 'COVID-19_Behavior_Tracker'
+
+    # Importing dataset
+    dataset_name = 'covid-19_behaviors-data'
     df = pd.read_csv(f"{dataset_name}.csv")
 
+    # Initial columns of interest
     target_cols = ["Entity", "Year",
                    "household_members_contact", "people_contact_outside_household", "times_left_home_yesterday", "handwashing_yesterday",
                    "willingness_isolate_if_symptoms", "difficult_to_isolate", "willingness_isolate_if_advised",
@@ -13,12 +15,12 @@ def main():
                    "avoided_people_with_symptoms", "avoided_going_out", "avoided_healthcare_settings", "avoided_public_transport",
                    "avoided_working_outside_home", "children_avoided_school", "avoided_guests_at_home", "avoided_small_gatherings",
                    "avoided_medium_gatherings", "avoided_large_gatherings", "avoided_crowded_areas", "avoided_shops",
-                   "cleaned_surfaces_home", "covered_mouth_sneeze", "hand_sanitiser", "handwashing_yesterday",
+                   "cleaned_surfaces_home", "covered_mouth_sneeze", "hand_sanitiser",
                     "scared_contracting_covid", "happier_two_weeks_ago",
                    "covid_dangerous_to_me", "likely_get_covid_future", "mask_protect_me", "mask_protect_others", "life_greatly_affected"]
 
-    # Removing years without n2o or methane data
-
+    # Removing entries with significant amount of missing data (as certain countries stopped reporting data)
+    # Done via manual identification
     df = df[target_cols]
     df_ = df.loc[df['Year'] < 500]
 
@@ -27,24 +29,27 @@ def main():
     df_norm['Entity'] = df['Entity']
     df_norm = df_norm.groupby(['Entity'], as_index=False).mean()
 
-
-
+    # Determining columns where <25% of data is missing (once grouped by country)
     thresholds = df_norm.loc[:, df_norm.columns != "Entity"].mean(axis=0)
     thresholded_cols = thresholds.loc[thresholds > 0.75]
     thresholded_headers = list(thresholded_cols.axes[0])
-
-    threshold_entity = df_norm[thresholded_headers].mean(axis=1)
-    thresholded_cols = threshold_entity.loc[threshold_entity > 0.9]
-
-    # Thresholding
-
-    # GDP
     thresholded_headers.insert(0, "Entity")
-    df_ = df_[thresholded_headers]
 
+    # Determining countries where remaining data metrics have <10% of data is missing
+    df_norm = df_norm[thresholded_headers]
+    thresholds = df_norm.loc[:, df_norm.columns != "Entity"].mean(axis=1)
+    thresholded_rows = thresholds.loc[thresholds > 0.9]
+    thresholded_country = list(df_norm.iloc[list(thresholded_rows.index)]['Entity'])
+
+    # Applying thresholds
+    df_ = df_[thresholded_headers]
+    df_ = df_[df_['Entity'].isin(thresholded_country)]
+
+    # Replacing Nans with 0s
     df_final = df_.fillna(0)
 
-    df_final.to_csv("covid-19_behaviors.csv", index=False)
+    # Exporting to csv
+    df_final.to_csv("covid-19_behaviors-data.csv", header=False, index=False)
 
 
 if __name__ == '__main__':
--- processed/emissions/emissions-processor.py
+++ processed/emissions/emissions-processor.py
@@ -2,19 +2,19 @@ import pandas as pd
 
 
 def main():
-    # dataset_name = 'YouGov-Imperial COVID-19 Behavior Tracker'
-    dataset_name = 'owid-co2-data'
-    df = pd.read_csv(f"datasets\{dataset_name}.csv")
 
+    # Importing dataset
+    dataset_name = 'emissions-data'
+    df = pd.read_csv(f"{dataset_name}.csv")
+
+    # Initial columns of interest
     target_cols = ["country", "year", "iso_code", "population", "gdp", "co2", "cement_co2", "coal_co2", "flaring_co2",
                    "gas_co2", "methane", "nitrous_oxide", "oil_co2", "share_global_cement_co2",
                    "share_global_co2", "share_global_coal_co2", "share_global_flaring_co2", "share_global_gas_co2",
                    "share_global_oil_co2"]
 
-    # Removing years without n2o or methane data
-
+    # Removing years without n2o or methane data (after manual id)
     df = df[target_cols]
-
     df = df.loc[(df['year'] > 1991) & (df['year'] < 2019)]  # TODO - add GPD data from external dataset for 2019-2020
 
     # Normalizing based on number of nan entries
@@ -22,41 +22,36 @@ def main():
     df_['country'] = df['country']
     df_ = df_.groupby(['country'], as_index=False).mean()
 
-    # Thresholding
-
-    # GDP
+    # Thresholding based on GPD and various C02 metrics
     df_ = df_.loc[df_['gdp'] > 0.9]
-
-    # Emission breakdowns
     df_ = df_.loc[((df_['cement_co2'] > 0.75) |
                    (df_['coal_co2'] > 0.75) |
                    (df_['gas_co2'] > 0.75))]
 
-    # Generic density check
+    # Generic density check across remaining metrics
     df_ = df_[df_.iloc[:, 1:].mean(axis=1) > 0.85]
 
     # Un-squeezing country and replacing nans with 0
     df_final = df.loc[df["country"].isin(df_['country'])].fillna(0)
 
-    # Calculating remaining co2 amount
+    # Calculating remaining co2 amount and replacing negative value with 0 # TODO - confirm if this is ok
     df_final['other_co2'] = df_final['co2'] - (df_final['oil_co2'] +
                                                df_final['gas_co2'] +
                                                df_final['coal_co2'] +
                                                df_final['cement_co2'] +
                                                df_final['flaring_co2'])
-
-    # Replacing negative value with 0 # TODO - confirm if this is ok
     df_final.loc[df_final['other_co2'] < 0, 'other_co2'] = 0
 
+    # Reordering output columns
     output_col_ordered = ['year', 'country', 'iso_code', 'gdp', 'population', 'methane', 'nitrous_oxide', 'cement_co2',
                           'coal_co2', 'gas_co2', 'oil_co2', 'flaring_co2', 'other_co2', 'co2',
                           'share_global_cement_co2', 'share_global_coal_co2', 'share_global_gas_co2',
                           'share_global_oil_co2',
                           'share_global_flaring_co2', 'share_global_co2']
     df_final = df_final[output_col_ordered]
-    
+
+    # Exporting to csv
     df_final.to_csv("emissions-corgis.csv", header=False, index=False)
-    print("")
 
 
 if __name__ == '__main__':
